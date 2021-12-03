67df8afda172e458f716d667f3a0db962cbbf2bb
Maldorana <maldorana@gmail.com>
2021-12-03 12:41:51 +0000

    Pass test for 1 minute

--- src/timeConverter.ts
+++ src/timeConverter.ts
@@ -1,3 +1,4 @@
 export function timeConverter(seconds: number): string {
+  if (seconds >= 60) return '1 minute';
   return seconds !== 0 ? `${seconds} second` + (seconds > 1 ? 's' : '') : 'now';
 }
--- test/timeConverter.test.ts
+++ test/timeConverter.test.ts
@@ -1,13 +1,21 @@
 import { timeConverter } from '../src/timeConverter';
 
 describe('timeConverter tests', () => {
-  it("Should return 'now' if entered 0", () => {
-    expect(timeConverter(0)).toBe('now');
+  describe('only seconds', () => {
+    it("Should return 'now' if entered 0", () => {
+      expect(timeConverter(0)).toBe('now');
+    });
+    it("Should return '1 second' if entered 1", () => {
+      expect(timeConverter(1)).toBe('1 second');
+    });
+    it("Should return '24 seconds' if entered 24", () => {
+      expect(timeConverter(24)).toBe('24 seconds');
+    });
   });
-  it("Should return '1 second' if entered 1", () => {
-    expect(timeConverter(1)).toBe('1 second');
-  });
-  it("Should return '24 seconds' if entered 24", () => {
-    expect(timeConverter(24)).toBe('24 seconds');
+
+  describe('minutes and seconds', () => {
+    it("Should return '1 minute' if entered 60", () => {
+      expect(timeConverter(60)).toBe('1 minute');
+    });
   });
 });
