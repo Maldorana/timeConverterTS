e40230e34734f6e2c979a0321258d876ba27f849
Maldorana <maldorana@gmail.com>
2021-12-04 15:36:13 +0000

    Refactoring

--- src/timeConverter.ts
+++ src/timeConverter.ts
@@ -1,46 +1,29 @@
+const time: any = {
+  year: 31536000,
+  day: 86400,
+  hour: 3600,
+  minute: 60,
+  second: 1,
+};
+
 export function timeConverter(seconds: number): string {
-  let time: string[] = [];
+  let convertedTime: string[] = [];
 
   if (seconds === 0) return 'now';
 
-  if (seconds >= 31536000) {
-    time.push(
-      `${Math.floor(seconds / 31536000)}` +
-        (Math.floor(seconds / 31536000) > 1 ? ' years' : ' year')
-    );
-    seconds = seconds % 31536000;
+  for (const key in time) {
+    if (seconds >= time[key]) {
+      let val: string =
+        `${Math.floor(seconds / time[key])}` +
+        ` ${key}` +
+        (Math.floor(seconds / time[key]) > 1 ? 's' : '');
+      convertedTime.push(val);
+      seconds = seconds % time[key];
+    }
   }
 
-  if (seconds >= 86400) {
-    time.push(
-      `${Math.floor(seconds / 86400)}` +
-        (Math.floor(seconds / 86400) > 1 ? ' days' : ' day')
-    );
-    seconds = seconds % 86400;
-  }
-
-  if (seconds >= 3600) {
-    time.push(
-      `${Math.floor(seconds / 3600)}` +
-        (Math.floor(seconds / 3600) > 1 ? ' hours' : ' hour')
-    );
-    seconds = seconds % 3600;
-  }
-
-  if (seconds >= 60) {
-    time.push(
-      `${Math.floor(seconds / 60)}` +
-        (Math.floor(seconds / 60) > 1 ? ' minutes' : ' minute')
-    );
-    seconds = seconds % 60;
-  }
-
-  if (seconds > 0) {
-    time.push(seconds.toString() + (seconds > 1 ? ' seconds' : ' second'));
-  }
-
-  return time.reduce(
+  return convertedTime.reduce(
     (previous, current, index) =>
-      previous + (index < time.length - 1 ? ', ' : ' and ') + current
+      previous + (index < convertedTime.length - 1 ? ', ' : ' and ') + current
   );
 }
